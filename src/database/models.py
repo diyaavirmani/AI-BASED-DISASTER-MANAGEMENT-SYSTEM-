1268a8d327fcb7e596cdc76baa9b6fbac436556d
diyavirmani41 <diyavirmani41@gmail.com>
2026-03-07 23:03:08 +0530

    feat(database): add ReportModel stub

--- src/database/models.py
+++ src/database/models.py
@@ -1,2 +1,8 @@
 # SQLAlchemy ORM models
 #this will store the final cleaned reports
+import logging
+
+class ReportModel:
+    """Stub for report model."""
+    def __init__(self):
+        logging.debug("ReportModel initialized")
