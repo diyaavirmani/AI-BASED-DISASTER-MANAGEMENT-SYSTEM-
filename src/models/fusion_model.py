0f8fb6f554cc38b8efab884882b02ff5e46b72e1
diyavirmani41 <diyavirmani41@gmail.com>
2026-03-07 23:02:28 +0530

    feat(models): add fusion stub with logging

--- src/models/fusion_model.py
+++ src/models/fusion_model.py
@@ -1,1 +1,7 @@
 # SAR + Optical fusion model placeholder
+import logging
+
+def fuse_data(sar, optical):
+    """Stub for fusing SAR and optical data."""
+    logging.debug("Fusing data")
+    return sar + optical
