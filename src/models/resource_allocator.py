3df8c05b0c504140831877282a3a86ded7269e27
diyavirmani41 <diyavirmani41@gmail.com>
2026-03-07 23:03:24 +0530

    feat(models): add resource allocator stub

--- src/models/resource_allocator.py
+++ src/models/resource_allocator.py
@@ -1,1 +1,7 @@
 # Routing & optimization placeholder
+import logging
+
+def allocate_resources(zones):
+    """Stub for resource allocation."""
+    logging.info("Allocating resources")
+    return zones
