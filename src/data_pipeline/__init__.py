20199f81c12bc26483986267318269ce1a9b33ca
diyavirmani41 <diyavirmani41@gmail.com>
2026-03-13 23:59:08 +0530

    Export social_image_analyzer in data_pipeline package

--- src/data_pipeline/__init__.py
+++ src/data_pipeline/__init__.py
@@ -1,5 +1,11 @@
 """Data pipeline package: satellite fetchers, preprocessors, index calculators."""
 
-from . import satellite_fetcher, crowdsource_fetcher, preprocessor, index_calculator
+from . import crowdsource_fetcher, index_calculator, preprocessor, satellite_fetcher, social_image_analyzer
 
-__all__ = ["satellite_fetcher", "crowdsource_fetcher", "preprocessor", "index_calculator"]
+__all__ = [
+    "crowdsource_fetcher",
+    "index_calculator",
+    "preprocessor",
+    "satellite_fetcher",
+    "social_image_analyzer",
+]
