561643f73130654540d1da16ca064ca16c9d8d62
diyaavirmani <diyavirmani41@gmail.com>
2026-05-03 16:00:00 +0530

    feat: Update dataset builder and configuration for multimodal training
    - Modify DisasterDataset to use new augmentation transforms
    - Add build_dataloaders with subset_size support for debug mode
    - Update config.yaml with model architecture switching
    - Add HRNet width and zone predictor parameters
    - Configure training hyperparameters and paths

--- src/data_pipeline/dataset_builder.py
+++ src/data_pipeline/dataset_builder.py
@@ -14,23 +14,17 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-
-def get_transform():
-    transform = A.Compose(
-        [
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.2),
-            A.Normalize()
-        ]
-    )
-    return transform
+from src.training.augmentation import get_training_transforms, get_validation_transforms
 
 class DisasterDataset(Dataset):
     def __init__(self, file_pairs, augment=False, config=None):
         self.file_pairs = file_pairs
         self.augment = augment
         self.config = config
-        self.transform = get_transform()
+        if augment:
+            self.transform = get_training_transforms()
+        else:
+            self.transform = get_validation_transforms()
     
     def __len__(self):
         return len(self.file_pairs)
@@ -41,67 +35,57 @@ class DisasterDataset(Dataset):
         image = np.load(image_path)
         # load mask
         mask = np.load(label_path)
-        # augmentations
-        if self.augment:
-            augmented = self.transform(image=image, mask=mask)
-            image = augmented["image"]
-            mask = augmented["mask"]
-        # convert to tensors
-        image = torch.from_numpy(image).float()
-        image = image.permute(2, 0, 1)
-        mask = torch.from_numpy(mask).long()
+        
+        # Apply transforms (includes augmentation if training, and tensor conversion)
+        transformed = self.transform(image=image, mask=mask)
+        image = transformed["image"]
+        mask = transformed["mask"]
+        
         return image, mask
+
+def build_dataloaders(config):
+    image_dir = config["data"]["image_dir"]
+    label_dir = config["data"]["label_dir"]
+    batch_size = config["training"]["batch_size"]
+    subset_size = config["data"].get("subset_size")
     
-    @staticmethod
-    def build_dataloaders(config):
-        image_dir = config["image_dir"]
-        label_dir = config["label_dir"]
-        batch_size = config["batch_size"]
+    file_pairs = []
+    
+    for name in os.listdir(image_dir):
+        img_path = os.path.join(image_dir, name)
+        label_path = os.path.join(label_dir, name)
         
-        file_pairs = []
-        
-        for name in os.listdir(image_dir):
-            img_path = os.path.join(image_dir, name)
-            label_path = os.path.join(label_dir, name)
-            
-            if os.path.exists(img_path) and os.path.exists(label_path):
-                file_pairs.append((img_path, label_path))
-        
-        train_pairs = []
-        val_pairs = []
-        test_pairs = []
-        
-        for img, lbl in file_pairs:
-            if "train" in img:
-                train_pairs.append((img, lbl))
-            elif "val" in img:
-                val_pairs.append((img, lbl))
-            else:
-                test_pairs.append((img, lbl))
-        
-        train_dataset = DisasterDataset(train_pairs, augment=True, config=config)
-        val_dataset = DisasterDataset(val_pairs, augment=False, config=config)
-        test_dataset = DisasterDataset(test_pairs, augment=False, config=config)
-        
-        train_loader = DataLoader(
-            train_dataset,
-            batch_size=batch_size,
-            shuffle=True,
-        )
-        
-        val_loader = DataLoader(
-            val_dataset,
-            batch_size=batch_size,
-            shuffle=False,
-        )
-        
-        test_loader = DataLoader(
-            test_dataset,
-            batch_size=batch_size,
-            shuffle=False,
-        )
-        
-        return train_loader, val_loader, test_loader
+        if os.path.exists(img_path) and os.path.exists(label_path):
+            file_pairs.append((img_path, label_path))
+    
+    if subset_size:
+        file_pairs = file_pairs[:subset_size]
+    
+    train_pairs = []
+    val_pairs = []
+    
+    for img, lbl in file_pairs:
+        if "train" in img:
+            train_pairs.append((img, lbl))
+        elif "val" in img:
+            val_pairs.append((img, lbl))
+    
+    train_dataset = DisasterDataset(train_pairs, augment=True, config=config)
+    val_dataset = DisasterDataset(val_pairs, augment=False, config=config)
+    
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+    
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+    
+    return train_loader, val_loader
 
 config = {
     "image_dir": "./data/images",
